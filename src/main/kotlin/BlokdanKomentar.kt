d8341901c8d4f39cb67d3db6c23c4e9dd3241cd6
Adinda Oktavia <adindaoktaviabks0812@gmail.com>
2024-09-06 18:26:11 +0700

    Add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama saya di Kotlin!")
+
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multibaris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
