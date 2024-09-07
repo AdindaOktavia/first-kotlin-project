1a5606a3a8250f421bad032a0ddc2cd592d1d67c
Adinda Oktavia <adindaoktaviabks0812@gmail.com>
2024-09-07 14:22:15 +0700

    Add RawString.kt

--- src/main/kotlin/RawString.kt
+++ src/main/kotlin/RawString.kt
@@ -1,10 +1,6 @@
 fun main(args: Array<String>) {
-    var varBool: Boolean = true
-    if (varBool)
-        println("varBool bernilai " + varBool)
-
-    var varInt: Int = 1
-    // Memeriksa apakah varInt bukan nol atau memiliki nilai tertentu
-    if (varInt != 0)
-        println("varInt bernilai " + varInt)
+    var text = """"
+        for (c in "foo")
+    """
+    println(text)
 }
